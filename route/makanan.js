9a656022ff7143760c67375552c0fdfabb935570
Daniel <sdanielsahat1632@gmail.com>
2023-12-07 23:16:11 +0700

    add makanan

---
+++ route/makanan.js
@@ -0,0 +1,51 @@
+var router = require('express').Router()
+var fire = require('../config/dbConfig')
+var bodyParser = require('body-parser')
+var db = fire.firestore()
+router.use(bodyParser.json())
+
+// get all makanan
+router.get('/makanan', (req, res)=>{
+    var makanan = []
+    db.collection('makanan').get()
+    .then((snapshot)=>{
+        snapshot.forEach((doc)=>{
+            makanan.push(doc.data())
+        })
+        res.json({
+            message: 'success',
+            data: makanan
+        })
+    })
+    .catch((err)=>{
+        res.json({
+            message: 'error',
+            data: err
+        })
+    })
+})
+
+// get makanan by nama_makanan
+router.get('/makanan/:nama_makanan', (req, res)=>{
+    var nama_makanan = req.params.nama_makanan
+    db.collection('makanan').where('nama_makanan', '==', nama_makanan).get()
+    .then((snapshot)=>{
+        var makanan = []
+        snapshot.forEach((doc)=>{
+            makanan.push(doc.data())
+        })
+        res.json({
+            message: 'success',
+            data: makanan
+        })
+    })
+    .catch((err)=>{
+        res.json({
+            message: 'error',
+            data: err
+        })
+    })
+})
+
+// export router
+module.exports = router
